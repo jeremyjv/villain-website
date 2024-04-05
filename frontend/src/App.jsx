a84cf315d41ba03f3a2a56d9352b8fecf7d80add
jeremyjv <jeremyjvillanueva@gmail.com>
2024-04-05 09:09:01 -0700

    got right gradient colors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,14 +22,15 @@ function App() {
         <div class="bg-black absolute inset-0 bg-cover bg-center">
           <div className= 'text-white text-bold font-body pl-2 pt-2'>jeremys-macbook-pro % git commit -m “its gotta be dark mode”</div>
           <div className= 'text-white text-bold font-body pl-2 pt-2'>jeremys-macbook-pro % git push</div>
-          <div className= 'absolute text-dark text-8xl text-transparent bg-clip-text bg-gradient-to-t from-blue-400 to-purple-800 text-center inset-x-0 bottom-0'>
-            <div className= 'absolute left-3'>Jeremy</div>
-            <div className= 'absolute left-3/8'>Jeremy</div>
-            <div className= 'absolute left-3/8'>Jeremy</div>
+          <div className= 'text-dark text-8xl text-transparent bg-clip-text bg-gradient-to-t from-blue-500 to-purple-600 text-center inset-x-0 bottom-0'>
+            <div className= ''>
+            <div className= 'absolute left-1'>Jeremy</div>
+            <div>Jeremy</div>
+            <div>Jeremy</div>
             <div>Villanueva</div>
             <div>Villanueva</div>
             <div>Villanueva</div>
-            
+            </div>
             </div>
         </div>
     </div>
