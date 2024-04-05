7a6170412c514a1fdecd4e446871c5d8f625b800
jeremyjv <jeremyjvillanueva@gmail.com>
2024-04-05 08:54:48 -0700

    added gradient

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,7 +20,17 @@ function App() {
    
    <div class="relative h-screen">
         <div class="bg-black absolute inset-0 bg-cover bg-center">
-          <div className= 'text-white text-bold font-body'>Jeremy Villanueva</div>
+          <div className= 'text-white text-bold font-body pl-2 pt-2'>jeremys-macbook-pro % git commit -m “its gotta be dark mode”</div>
+          <div className= 'text-white text-bold font-body pl-2 pt-2'>jeremys-macbook-pro % git push</div>
+          <div className= 'absolute text-dark text-8xl text-transparent bg-clip-text bg-gradient-to-t from-blue-400 to-purple-800 text-center inset-x-0 bottom-0'>
+            <div className= 'absolute left-3'>Jeremy</div>
+            <div className= 'absolute left-3/8'>Jeremy</div>
+            <div className= 'absolute left-3/8'>Jeremy</div>
+            <div>Villanueva</div>
+            <div>Villanueva</div>
+            <div>Villanueva</div>
+            
+            </div>
         </div>
     </div>
 
